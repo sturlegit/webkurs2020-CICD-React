de6597e6e6a5b2cdadd59e522d762505865cf17c
Orakelpc <orakelpc@Orakelpcs-MBP.home>
2020-10-21 17:19:29 +0200

    Updates

---
+++ precache-manifest.8d457c480f8d642ce710702ef4845b17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b467d430471748d989c079df26e82c",
+    "url": "/webkurs2020-CICD-React/index.html"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/webkurs2020-CICD-React/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "271b4dab529f1ee515ca",
+    "url": "/webkurs2020-CICD-React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/webkurs2020-CICD-React/static/js/2.16ab8709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webkurs2020-CICD-React/static/js/2.16ab8709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "271b4dab529f1ee515ca",
+    "url": "/webkurs2020-CICD-React/static/js/main.acaf86c3.chunk.js"
+  },
+  {
+    "revision": "580e99ae8bc13b712167",
+    "url": "/webkurs2020-CICD-React/static/js/runtime-main.89e14c0b.js"
+  }
+]);
